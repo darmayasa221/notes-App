39c959dac14e20068ea2cb175f1e87a888079c2d
Darma Yasa <darmayasa@192.168.204.102>
2022-04-02 00:35:41 +0800

    notes-app

---
+++ src/api/notes/index.js
@@ -0,0 +1,11 @@
+const NotesHandler = require('./handler');
+const rootes = require('./routes');
+
+module.exports = {
+  name: 'notes',
+  version: '1.0.0',
+  register: async (server, { service, validator }) => {
+    const notesHandler = new NotesHandler(service, validator);
+    server.route(rootes(notesHandler));
+  },
+};
